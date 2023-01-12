9a7a772d239e1d0058691ca5235f89c73d7a663b
greidinger.dev <jp@greidinger.dev>
2023-01-12 20:45:45 -0300

    fixed loading,login ui

--- src/components/ui/Loading.tsx
+++ src/components/ui/Loading.tsx
@@ -1,5 +1,5 @@
 import {CgSpinner} from "react-icons/cg";
 
 export function Loading() {
-    return <CgSpinner className="animate-spin text-2xl text-secondary"/>;
+    return <CgSpinner className="animate-spin text-2xl text-primary"/>;
 }
--- src/components/ui/Login.tsx
+++ src/components/ui/Login.tsx
@@ -2,12 +2,12 @@ import {signIn} from "next-auth/react";
 
 export function Login() {
     return (
-        <div>
-            <button className="btn">Login with Google</button>
-            <button className="btn">Login with Facebook</button>
-            <button className="btn">Login with Discord</button>
+        <div className="h-screen flex justify-center items-center">
+            {/*<button className="btn">Login with Google</button>*/}
+            {/*<button className="btn">Login with Facebook</button>*/}
+            {/*<button className="btn">Login with Discord</button>*/}
             <button onClick={() => signIn("discord")} className="btn">
-                Login with Github
+                Login with Discord
             </button>
         </div>
     );
